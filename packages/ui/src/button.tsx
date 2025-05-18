4cac3b7655f3fc687f550b6771ce5c65ad0a74ff
Divyesh Radadiya <divyeshradadiya21@gmail.com>
2025-05-18 10:43:19 +0530

    update : pnpm lock file

--- packages/ui/src/button.tsx
+++ packages/ui/src/button.tsx
@@ -1,29 +1,37 @@
-import * as React from "react";
 import { Slot } from "@radix-ui/react-slot";
-import { cva, type VariantProps } from "class-variance-authority";
 import { cn } from "@repo/lib";
+import { type VariantProps, cva } from "class-variance-authority";
+import * as React from "react";
+
 
 const buttonVariants = cva(
-  "inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0",
+  "inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50",
   {
     variants: {
       variant: {
         default:
-          "bg-primary text-primary-foreground shadow hover:bg-primary/90",
+          "rounded-[6px] bg-[#1155ff] text-primary-foreground shadow hover:bg-blue-800",
+        blue: "rounded-[6px] bg-[#1155ff] dark:bg-blue-500 text-primary-foreground shadow hover:bg-blue-800 dark:text-white",
+        gray: "bg-gray-200 text-black hover:bg-gray-300 dark:bg-gray-900 dark:text-white",
+        whiteblue:
+          "hover:bg-gray-100 bg-white text-[#1155ff] rounded-lg shadow-md",
         destructive:
-          "bg-destructive text-destructive-foreground shadow-sm hover:bg-destructive/90",
+          "bg-destructive text-destructive-foreground shadow-sm hover:bg-red-600",
         outline:
-          "border border-input bg-background shadow-sm hover:bg-accent hover:text-accent-foreground",
+          "border border-input bg-transparent shadow-sm hover:bg-accent hover:text-accent-foreground",
         secondary:
           "bg-secondary text-secondary-foreground shadow-sm hover:bg-secondary/80",
         ghost: "hover:bg-accent hover:text-accent-foreground",
         link: "text-primary underline-offset-4 hover:underline",
+        navbarIcon:
+          "m-0 p-1 transition-all duration-300 ease-in-out hover:bg-accent hover:text-accent-foreground",
+        user: "flex items-center justify-center",
       },
       size: {
-        default: "h-9 px-4 py-2",
+        default: "h-9 px-4 py-2 gap-2",
         sm: "h-8 rounded-md px-3 text-xs",
         lg: "h-10 rounded-md px-8",
-        icon: "h-9 w-9",
+        icon: "size-9 rounded-full text-sm",
       },
     },
     defaultVariants: {
